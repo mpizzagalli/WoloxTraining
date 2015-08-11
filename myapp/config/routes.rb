3f27e7d1bf4f8cbcfae10e882365ce61da89a5db
Matias Pizzagalli <matias.pizzagalli@wolox.com.ar>
2015-08-11 12:03:49 -0300

    Fix in routes.db, we had duplicate code

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,13 +7,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   #root 'welcome#index'
 
-  Rails.application.routes.draw do
- 
+  devise_for :users do
     resources :articles do
       resources :comments
     end
- 
-    root 'welcome#index'
+
   end
 
   # Example of regular route:
