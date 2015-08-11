8b237b10205564530c257af6d973bcd9f22d0da9
Matias Pizzagalli <matias.pizzagalli@wolox.com.ar>
2015-08-11 09:49:30 -0300

    refactorized to use memorization in comments controller

--- myapp/app/controllers/comments_controller.rb
+++ myapp/app/controllers/comments_controller.rb
@@ -1,13 +1,11 @@
 class CommentsController < ApplicationController
 	def create
-		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create(comment_params)
+		@comment = article.comments.create(comment_params)
 		redirect_to article_path(@article)
 	end
 
 	def destroy
-		@article = Article.find(params[:article_id])
-		@comment = @article.comments.find(params[:id])
+		@comment = article.comments.find(params[:id])
 		@comment.destroy
 		redirect_to article_path(@article)
 	end
@@ -15,5 +13,9 @@ class CommentsController < ApplicationController
 	private
 	def comment_params
 		params.require(:comment).permit(:commenter, :body)
+	end
+
+	def article
+		@article ||= Article.find(params[:article_id])
 	end
 end
