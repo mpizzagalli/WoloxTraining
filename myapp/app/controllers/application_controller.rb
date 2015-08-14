1182d0d52726cafa9a94dab184070aee127e6838
Matias Pizzagalli <matias.pizzagalli@wolox.com.ar>
2015-08-14 12:45:33 -0300

    Fix from PR 9

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -5,11 +5,6 @@ class ApplicationController < ActionController::Base
     I18n.locale = extract_locale_from_tld || I18n.default_locale
   end
 
-  def extract_locale_from_tld
-    parsed_locale = params[:locale]
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
-  end
-
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
   end
@@ -17,4 +12,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def extract_locale_from_tld
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
